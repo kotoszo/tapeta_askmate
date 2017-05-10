a0ae66e5b11f564d22f76ed970937163c6ea50b4
Peter Juhasz <jpeter82@gmail.com>
2017-05-10 17:03:53 +0200

    finished display_questions in logic

--- logic.py
+++ logic.py
@@ -1,19 +1,25 @@
 import file_io
 import config
 import base64
+import datetime
+import time
 
 
 def display_questions():
-    order_by = 1
+    result = []
     questions = file_io.read_from_file(config.questions)
     if len(questions):
-        result = sorted(file_io.read_from_file(config.questions), key=lambda x: -int(x[1]))
+        questions = sorted(questions, key=lambda x: -int(x[1]))
+        for line in questions:
+            result = [[line[0], date_convert(line[1]), line[2], line[3], b64_convert(line[4]),
+                      file_io.change_eol(b64_convert(line[5]))] for line in questions]
     else:
         result = False
+
     return result
 
 
-def display_question():
+def display_question(id):
     pass
 
 
@@ -21,11 +27,21 @@ def sort_table():
     pass
 
 
-def b64_convert(text, mode=1):
-    if mode == 1:
+def b64_convert(text, decode=False):
+    if decode is True:
         result = base64.b64encode(text.encode("utf-8"))
-    elif mode == 2:
+    elif decode is False:
         result = base64.b64decode(text).decode("utf-8")
+    else:
+        raise ValueError
+    return result
+
+
+def date_convert(text, decode=False):
+    if decode is True:
+        result = int(time.mktime(text.timetuple()))
+    elif decode is False:
+        result = datetime.datetime.fromtimestamp(int(text)).strftime("%Y-%m-%d %H:%M:%S")
     else:
         raise ValueError
     return result
@@ -36,3 +52,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+    print(display_questions())
+    '''
+    dt = date_convert(1284101485, mode=2)
+    print(dt)
+    dt2 = date_convert(dt, mode=1)
+    print(dt2)
+    '''
+
