0863d9dfb64485de30853437eed2dec48a4f3c8d
rtomil <rtomilobil@gmail.com>
2017-05-10 12:42:26 +0200

    example base64 and variables added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, url_for
 import file_io
+import config
+import base64
 
 app = Flask(__name__)
 
@@ -13,19 +15,28 @@ def base():
 @app.route('/new-question')
 @app.route('/new-answer')
 def form():
-    return render_template('form.html')
+    answers = file_io.read_from_file(config.answers)
+    questions = file_io.read_from_file(config.questions)
+    return render_template('form.html', answer=answers, question=questions)
 
 
 @app.route('/question/<question_id>')
 def question():
-    return render_template('question.html')
+    questions = file_io.read_from_file(config.questions)
+    answers = file_io.read_from_file(config.answers)
+    return render_template('question.html', question=questions, answer=answers)
 
 
 @app.route('/')
 @app.route('/list')
 def index():
-    return render_template('index.html')
+    questions = file_io.read_from_file(config.questions)
+    return render_template('index.html', questions=questions)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    encoded = base64.b64encode("This is something, you wont read again.".encode())
+    print(encoded)
+    decoded = base64.b64decode(encoded.decode())
+    print(decoded)
