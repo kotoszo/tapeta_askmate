386b2cd6a675c211474739f7db12b8d23fa9104e
rtomil <rtomilobil@gmail.com>
2017-05-10 11:58:17 +0200

    app.sources added

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from flask import Flask, request, render_template, url_for
+import file_io
 
 app = Flask(__name__)
 
 
-@app.route('/')
 def base():
     return render_template('base.html')
 
 
-@app.route('/')
+@app.route('/question/<question_id>/edit')
+@app.route('/answer/<answer_id>/edit')
+@app.route('/new-question')
+@app.route('/new-answer')
 def form():
     return render_template('form.html')
 
 
-@app.route('/')
+@app.route('/question/<question_id>')
 def question():
     return render_template('question.html')
 
 
 @app.route('/')
+@app.route('/list')
 def index():
     return render_template('index.html')
 
