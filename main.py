8c3afff2af0dc7de5c5ed05102e24b6a5a464481
kelliartamas <kelliar.tamas@gmail.com>
2017-05-11 10:45:03 +0200

    removed answers from main func, index.html added button for open question

--- main.py
+++ main.py
@@ -14,17 +14,14 @@ def base():
 @app.route('/<theme>', methods=['GET', 'POST'])
 @app.route('/<theme>/<item_id>/edit', methods=['GET', 'POST'])
 def form(question_id=None, answer_id=None, theme=None, item_id=None):
-    answers = logic.display_answers()
     display = logic.display_questions()
-    return render_template('form.html', theme=theme, question=display, answers=answers,
-                           answer_id=answer_id, question_id=question_id, item_id=item_id)
+    return render_template('form.html', theme=theme, question=display, answer_id=answer_id, question_id=question_id, item_id=item_id)
 
 
 @app.route('/question/<question_id>')
 def question(question_id):
     questions = logic.display_questions()
-    answers = logic.display_answers()
-    return render_template("question.html", question_id=question_id, questions=questions, answers=answers)
+    return render_template("question.html", question_id=question_id, questions=questions)
 
 
 @app.route('/', methods=['GET', 'POST'])
